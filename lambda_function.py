815ef283607422f00743fafef777e49c8e155611
Yurich <yuriy.timchuk@cheitgroup.com>
2024-10-05 13:57:41 +0100

    feat: add paramenters

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,21 @@
-import json
 from db_service import DbService
 
+BANK_MONO = 'Mono'
+BANK_PRIVAT = 'Privat'
+
+CURRENCY_EUR = 'EUR'
+CURRENCY_USD = 'USD'
+
 def lambda_handler(event, context):
-    items = DbService().get_data('Privat', 'USD')
+    bank = event['queryStringParameters']['bank']
+    currency = event['queryStringParameters']['currency']
+
+    validation_error = validate_parameters(bank, currency)
+    if validation_error:
+        return validation_error
+
+    items = DbService().get_data(bank, currency)
+
     return {
         'statusCode': 200,
         'headers': {
@@ -12,5 +25,39 @@ def lambda_handler(event, context):
         'body': items
     }
 
+def validate_parameters(bank, currency):
+    if not bank or not currency:
+        return {
+            'statusCode': 400,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            },
+            'body': 'Missing bank or currency'
+        }
+    
+    if bank not in [BANK_MONO, BANK_PRIVAT]:
+        return {
+            'statusCode': 422,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            },
+            'body': 'Not supported bank'
+        }
+    
+    if currency not in [CURRENCY_USD, CURRENCY_EUR]:
+        return {
+            'statusCode': 422,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            },
+            'body': 'Not supported currency'
+        }
+    
+    return None
+
 if __name__ == '__main__':
-    lambda_handler(None, None)
+    event = { 'queryStringParameters': { 'bank': 'Privat', 'currency': 'USD' } }
+    lambda_handler(event, None)
